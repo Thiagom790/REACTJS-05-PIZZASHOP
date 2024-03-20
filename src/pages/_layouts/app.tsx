964ea98bcc099754e180c1f496a34cd0afb221ab
thiago teixeira <thiagom790@gmail.com>
2024-03-20 11:05:40 -0300

    feat: logof user

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,8 +1,34 @@
-import { Outlet } from 'react-router-dom'
+import { isAxiosError } from 'axios'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import { Header } from '@/components/header'
+import { api } from '@/lib/axios'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      // Callback que vai ser executado sempre que o axios responder uma resposta de sucesso
+      (response) => response,
+      // Callback que vai ser executado sempre que o axios responder uma resposta de erro
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data?.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('/sign-in', { replace: true })
+          }
+        }
+      },
+    )
+
+    // Limpar o interceptador quando o componente for desmontado
+    return () => api.interceptors.response.eject(interceptorId)
+  }, [navigate])
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
